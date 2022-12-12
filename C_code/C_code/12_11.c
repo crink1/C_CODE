9f4aaa96b4e9290965821db780f8e954d240bb9f
crin <1575460882@qq.com>
2022-12-12 20:51:00 +0800

    数组

--- C_code/C_code/12_11.c
+++ C_code/C_code/12_11.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
-
-typedef struct MyStruct//结构体
-{
-	int age;
-	char name[10];
-	char id[20];
-	float score;
-}ms;
-
-int main()
-{
-	ms std = { 18,"张三","20221211",90 };
-	printf("%d",std.age);
-	return 0;
-}
+//#include<stdio.h>
+//
+//typedef struct MyStruct//结构体
+//{
+//	int age;
+//	char name[10];
+//	char id[20];
+//	float score;
+//}ms;
+//
+//int main()
+//{
+//	ms std = { 18,"张三","20221211",90 };
+//	printf("%d",std.age);
+//	return 0;
+//}
---
+++ C_code/C_code/12_12.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	
+	int arr[10] = { 0 };
+	//0~9
+	int i = 0;
+	while (i < 10)
+	{
+		scanf("%d", &arr[i]);//
+		i++;
+	}
+	//´òÓ¡
+	i = 0;
+	while (i < 10)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
+
+	return 0;
+}
