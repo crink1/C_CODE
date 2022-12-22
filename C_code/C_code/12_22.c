9800d14db536ce4232776153e107e13799d3e811
crin <1575460882@qq.com>
2022-12-22 19:16:28 +0800

    while

---
+++ C_code/C_code/12_22.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+//
+//int main() {
+//    int iq = 0;
+//    //ÊäÈë
+//    //scanf("%d", &iq);
+//    //if(iq >= 140)
+//    //    printf("Genius\n");
+//
+//    /*
+//    while(scanf("%d", &iq) == 1)
+//    {
+//        if(iq >= 140)
+//            printf("Genius\n");
+//    }
+//    */
+//
+//    while (scanf("%d", &iq) != EOF)
+//    {
+//        if (iq >= 140)
+//            printf("Genius\n");
+//    }
+//
+//    return 0;
+//}
