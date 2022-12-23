7ad6783c19437923393fe5baeb5c4223927d9db6
crin <1575460882@qq.com>
2022-12-23 11:03:14 +0800

    sizeof

---
+++ C_code/C_code/12_23.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//int main()
+//{
+//	//int a = 10;
+//	//printf("%d\n", sizeof(a));//4 - 单位是字节
+//	//printf("%d\n", sizeof(int));//4
+//
+//	int arr[10] = {0};
+//	printf("%d\n", sizeof(arr));//计算的是数组的总大小，单位是字节 40
+//	printf("%d\n", sizeof(arr[0]));
+//
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	printf("%d\n", sz);
+//
+//	return 0;
