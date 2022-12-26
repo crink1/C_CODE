ef04abe26ecc4377a82162d5e1d801592490e5f7
crin <1575460882@qq.com>
2022-12-26 09:32:23 +0800

    ++

---
+++ C_code/C_code/12_26.c
@@ -0,0 +1,26 @@
+//int main()
+//{
+//	//int a = 10;
+//	//int b = a--;//后置--，先使用，再--
+//	//printf("%d\n", a);
+//	//int b = a, a=a-1;
+//	//printf("a=%d b=%d\n", a, b);//9 10
+//
+//	//int a = 10;
+//	//int b = --a;//前置--，先--，再使用
+//	////a=a-1,int b=a;
+//	//printf("a=%d b=%d\n", a, b);
+//
+//
+//	//int a = 10;
+//	//int b = a++;//后置++，先使用，再++
+//	//int b = a, a = a+1;
+//	//printf("a=%d b=%d\n", a, b);//11 10
+//	
+//	int a = 10;
+//	int b = ++a;//
+//	
+//	printf("a=%d b=%d\n", a, b);//11 11
+//	return 0;
+//}
+//
