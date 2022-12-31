fa20a8b7d2b4c361ccb6405e57ada742a24f100c
crin <1575460882@qq.com>
2022-12-31 17:38:37 +0800

    枚举

---
+++ C_code/C_code/12_31.c
@@ -0,0 +1,27 @@
+//枚举常量
+//枚举 - enum
+//
+//性别： 男、女、保密
+//三原色：Red Green Blue
+//血型 ...
+//
+
+//声明的一个表示性别的枚举类型
+//enum Sex
+//{
+//	//下面三个符号是Sex这个枚举类型的未来的可能取值
+//	//枚举常量
+//	MALE=5,  //0
+//	FEMALE,//1
+//	SECRET //2
+//};
+//
+//int main()
+//{
+//	//enum Sex s = FEMALE;
+//	printf("%d\n", MALE);
+//	printf("%d\n", FEMALE);
+//	printf("%d\n", SECRET);
+//	//MALE = 5;
+//
+//	return 0;
