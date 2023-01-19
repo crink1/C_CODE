443f168ce87d6ca36b1b5fdd857402f164d86cef
crin <1575460882@qq.com>
2023-01-19 19:53:56 +0800

    数9的个数

---
+++ C_code/C_code/1_15.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int max = 0;
+//	int arr[10] = { 3,4,5,6,7,1,8,9,2,0 };
+//	for (i = 0; i < 10;i++)
+//	{
+//		if (arr[i] > max)
+//		{
+//			max = arr[i];
+//		}
+//	}
+//	printf("×î´óÖµ£º%d",max);
+//
+//	return 0;
+//}
+//int main()
+//{
+//	double sum = 0;
+//	double flag = 1;
+//	int i = 0;
+//	for (i = 1; i <=100; i++)
+//	{
+//		sum += (1.0/i)*flag;
+//		flag = -flag;
+//
+//
+//	}
+//	printf("%lf",sum);
+//
+//	return 0;
+//}
