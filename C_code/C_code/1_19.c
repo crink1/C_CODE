30af098a4ca8e3a0361dee256ce02fe4839e99d0
crin <1575460882@qq.com>
2023-01-20 22:49:15 +0800

    猜数字游戏 rand srand函数应用

--- C_code/C_code/1_19.c
+++ C_code/C_code/1_19.c
@@ -1,24 +1,24 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-int main()
-{
-	int i = 0;
-	int count = 0;
-	int x = 0;
-	for (i = 1; i <= 100; i++)
-	{
-		x = i;
-		while (x != 0)
-		{
-			if (x % 10 ==9)
-			{
-				count++;
-			}
-			x = x / 10;
-		}
-		
-		
-	}
-	printf("%d",count);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	int x = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		x = i;
+//		while (x != 0)
+//		{
+//			if (x % 10 ==9)
+//			{
+//				count++;
+//			}
+//			x = x / 10;
+//		}
+//		
+//		
+//	}
+//	printf("%d",count);
+//	return 0;
+//}
---
+++ C_code/C_code/1_20.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+
+
+void memu()
+{
+	printf("1.play\n0.exit\n");
+}
+
+
+void game()
+{
+	int ret = rand() % 100 + 1;
+	int guess = 0;
+	while (1)
+	{
+		printf("输入你猜的数字：\n");
+		scanf("%d", &guess);
+		if (guess > ret)
+		{
+			printf("猜大了\n");
+		}
+		else if (guess < ret)
+		{
+			printf("猜小了\n");
+		}
+		else
+		{
+			printf("猜对了\n");
+			break;
+		}
+
+
+	}
+}
+int main()
+{
+	srand((unsigned int)time(NULL));
+	int x = 0;
+	
+	do
+	{
+		memu();
+		printf("请选择：\n");
+		scanf("%d", &x);
+		switch (x)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			break;
+		default:
+			printf("输入错误\n");
+			break;
+		}
+	} while (x);
+	
+		
+	return 0;
+}
