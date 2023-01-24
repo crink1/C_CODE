f05620bcf9cb1d63391f01b9a65e55fb7bae1c32
crin <1575460882@qq.com>
2023-01-24 19:58:39 +0800

    函数交换位置

--- C_code/C_code/1_22.c
+++ C_code/C_code/1_22.c
@@ -1,18 +1,18 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-
-int main()
-{
-	int i = 0, j = 0, k = 0,c = 0;
-	scanf("%d", &k);
-	for (i = 1; i <= k; i++)
-	{
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d*%d=%d ",i,j,i*j);
-		}
-		printf("\n");
-	}
-	
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0, j = 0, k = 0,c = 0;
+//	scanf("%d", &k);
+//	for (i = 1; i <= k; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d ",i,j,i*j);
+//		}
+//		printf("\n");
+//	}
+//	
+//	return 0;
+//}
---
+++ C_code/C_code/1_23.c
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//
+//void swap(int *a,int *b)
+//{
+//	*a = *a + *b;
+//	*b = *a - *b;
+//	*a = *a - *b;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 1;
+//	printf("交换前%d %d ", a, b);
+//	swap(&a,&b);
+//	printf("交换后%d %d ", a, b);
+//	return 0;
+//}
