35a96024e42e603c171cf7b41fad0948ec5475d9
crin <1575460882@qq.com>
2023-01-25 21:00:11 +0800

    函数判断闰年

---
+++ C_code/C_code/1_24.c
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//
+//int leap(int year)
+//{
+//	return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+//}
+//
+//int main()
+//{
+//	int year = 0;
+//	printf("输入年份：");
+//	scanf("%d",&year);
+//	if (leap(year))
+//	{
+//		printf("是闰年");
+//	}
+//	else
+//	{
+//		printf("不是闰年");
+//	}
+//	return 0;
+//}
