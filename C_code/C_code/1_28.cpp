13d2a2ae05eb5fb5b07ef126849f34ab002060f0
crin <1575460882@qq.com>
2023-01-28 22:07:11 +0800

    递归正向求数字每一位

---
+++ C_code/C_code/1_28.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int num(int n)
+{
+	if (n > 9)
+	{
+		num(n / 10);
+	}
+	printf("%d  ", n % 10);
+
+}
+
+int main()
+{
+	int i = 0, n = 0;
+	int s = 1;
+	scanf("%d", &n);
+	num(n);
+	return 0;
+}
