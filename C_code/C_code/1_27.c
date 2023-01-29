39ce61dcf73980ff8ac6325f4c39eccbb49b8391
crin <1575460882@qq.com>
2023-01-29 22:05:24 +0800

    交换数组

--- C_code/C_code/1_27.c
+++ C_code/C_code/1_27.c
@@ -155,21 +155,21 @@
 //}
 
 
-int num(int n)
-{
-	if (n > 9)
-	{
-		num(n / 10);
-	}
-	printf("%d  ", n % 10);
-
-}
-
-int main()
-{
-	int i = 0, n = 0;
-	int s = 1;
-	scanf("%d", &n);
-	num(n);
-	return 0;
-}
+//int num(int n)
+//{
+//	if (n > 9)
+//	{
+//		num(n / 10);
+//	}
+//	printf("%d  ", n % 10);
+//
+//}
+//
+//int main()
+//{
+//	int i = 0, n = 0;
+//	int s = 1;
+//	scanf("%d", &n);
+//	num(n);
+//	return 0;
+//}
--- C_code/C_code/1_28.cpp
+++ C_code/C_code/1_28.cpp
@@ -1,20 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-int num(int n)
-{
-	if (n > 9)
-	{
-		num(n / 10);
-	}
-	printf("%d  ", n % 10);
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int num(int n)
+//{
+//	if (n > 9)
+//	{
+//		num(n / 10);
+//	}
+//	printf("%d  ", n % 10);
+//
+//}
+//
+//int main()
+//{
+//	int i = 0, n = 0;
+//	int s = 1;
+//	scanf("%d", &n);
+//	num(n);
+//	return 0;
+//}
 
-}
 
-int main()
-{
-	int i = 0, n = 0;
-	int s = 1;
-	scanf("%d", &n);
-	num(n);
-	return 0;
-}
