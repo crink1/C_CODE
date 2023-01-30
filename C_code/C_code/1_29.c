5126c174916222cb93a4f1082297cb6a28a3e6db
crin <1575460882@qq.com>
2023-01-30 19:37:31 +0800

    数组逆序

--- C_code/C_code/1_29.c
+++ C_code/C_code/1_29.c
@@ -1,20 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-int main()
-{
-    int i = 0;
-	int arr1[10] = { 1,2,3,4,5,6,7,8,9,0 };
-	int arr2[10] = { 0,9,8,7,6,5,4,3,2,1 };
-	int temp[10];
-    for (i = 0; i < 10; i++)
-    {
-        temp[i] = arr1[i];
-        arr1[i] = arr2[i];
-        arr2[i] = temp[i];
-    }
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d ", arr1[i]);
-    }
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//    int i = 0;
+//	int arr1[10] = { 1,2,3,4,5,6,7,8,9,0 };
+//	int arr2[10] = { 0,9,8,7,6,5,4,3,2,1 };
+//	int temp[10];
+//    for (i = 0; i < 10; i++)
+//    {
+//        temp[i] = arr1[i];
+//        arr1[i] = arr2[i];
+//        arr2[i] = temp[i];
+//    }
+//    for (i = 0; i < 10; i++)
+//    {
+//        printf("%d ", arr1[i]);
+//    }
+//	return 0;
+//}
---
+++ C_code/C_code/1_30.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+void reverse(int* parr,int sz)
+{
+    int left = 0;
+    int right = sz - 1;
+    while (left < right)
+    {
+        parr[left] = parr[left] + parr[right];
+        parr[right] = parr[left] - parr[right];
+        parr[left] = parr[left] - parr[right];
+        left++;
+        right--;
+    }
+}
+
+int main()
+{
+    int i = 0;
+    int arr1[10] = {0};
+    
+    for (i = 0; i < 10; i++)
+    {
+       scanf("%d", &arr1[i]);
+    }
+    int sz = sizeof(arr1) / sizeof(arr1[0]);
+    reverse(arr1,sz);
+    for (i = 0; i < 10; i++)
+    {
+       printf("%d ", arr1[i]);
+    }
+
+    return 0;
+}
