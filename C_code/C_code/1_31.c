21774c8ecf7373339dd3ff0d597b26e75c270078
crin <1575460882@qq.com>
2023-02-01 18:37:53 +0800

    三子棋

--- C_code/C_code/1_31.c
+++ C_code/C_code/1_31.c
@@ -1,27 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
-
-int main()
-{
-	int arr[10] = { 2,4,6,8,0,1,3,5,7,9 };
-	int i = 0;
-	int j = 0;
-	for (i=0;i<10-1;i++)
-	{
-		for (j = 0; j<10- i-1; j++)
-		{
-			if (arr[j] > arr[j + 1])
-			{
-				arr[j] = arr[j] + arr[j + 1];
-				arr[j+1] = arr[j] - arr[j + 1];
-				arr[j] = arr[j] - arr[j + 1];
-			}
-		}
-	}
-
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int arr[10] = { 2,4,6,8,0,1,3,5,7,9 };
+//	int i = 0;
+//	int j = 0;
+//	for (i=0;i<10-1;i++)
+//	{
+//		for (j = 0; j<10- i-1; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				arr[j] = arr[j] + arr[j + 1];
+//				arr[j+1] = arr[j] - arr[j + 1];
+//				arr[j] = arr[j] - arr[j + 1];
+//			}
+//		}
+//	}
+//
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
---
+++ C_code/C_code/2_1.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main()
+{
+	int input = 0;
+	do
+	{
+		printf("«Î ‰»Î£∫");
+		scanf("%d", &input);
+		switch (input)
+		{
+			case 1:
+				break;
+			case 0:
+				break;
+			default:
+				printf("—°‘Ò¥ÌŒÛ\n");
+				break;
+		}
+	} while (1);
+	
+	return 0;
+}
