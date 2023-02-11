114700ac0012170b61a6df7a8b1748b7b8f9aa71
crin <1575460882@qq.com>
2023-02-11 20:19:27 +0800

    刷题

---
+++ C_code/C_code/2.11.c
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<math.h>
+
+//void print(int arr[])
+//{
+//	int* parr = arr;
+//	for (int i = 0; i < 6; i++)
+//	{
+//		printf("%d",*(parr+i));
+//	}
+//}
+//
+//int main()
+//{
+//
+//	int arr[] = {1,2,3,4,5,6};
+//	print(arr);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//
+//	int i = 0;
+//	int j = 0;
+//	int k = 0;
+//	for (i = -7; i <= 7; i++)
+//	{
+//		for (j = 0; j < abs(i); j++)
+//		{
+//			printf(" ");
+//		}
+//		for (k = 0; k <15-abs(i)*2;k++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//
+//	int i = 0;
+//	for (i = 0; i <= 100000; i++)
+//	{
+//		int x = i;
+//		int n = 0;
+//		int sum = 0;
+//		while (x > 0)
+//		{
+//			x = x / 10;
+//			n++;
+//		}
+//		x = i;
+//		while (x>0)
+//		{
+//			sum += pow(x % 10, n);
+//			x = x / 10;
+//		}
+//		if (sum == i)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	return 0;
+//}
+
+
+
+int main()
+{
+
+	int i = 1;
+	int x = 2;
+	int sum = 0;
+	int n = x;
+	while (i<=5)
+	{
+		sum += n;
+		n = n * 10 + x;
+		i++;
+	}
+	printf("%d", sum);
+	return 0;
+}
---
+++ C_code/C_code/2_10.c
@@ -0,0 +1,19 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x = 15;
+//	int i = 0;
+//	printf("奇数位：\n");
+//	for (i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d", x >> i & 0x01);
+//	}
+//	printf("偶数位");
+//	for (i = 31; i >= 0; i -= 2)
+//	{
+//		printf("%d", x >> i & 0x01);
+//	}
+//	return 0;
+//}
