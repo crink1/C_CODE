6ef916681ef8cc5238627626ae76e4d07312e878
crin <1575460882@qq.com>
2023-02-14 20:17:37 +0800

    c

---
+++ C_code/C_code/2_14.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<assert.h>
+void my_strcpy(const char* arr1,char* arr2)
+{
+	assert(arr1);
+	assert(arr2);
+	while (*arr2++ = *arr1++)
+	{
+		;
+	}
+}
+
+
+
+int main()
+{
+
+	char arr1[]="abcdef";
+	char arr2[100];
+
+	my_strcpy(arr1,arr2);
+	printf("%s\n", arr2);
+	return 0;
+}
